a4b3dc1f23b8fdd1bc210418db807f143665d709
Andrey Korolev <f156@bk.ru>
2018-11-28 05:24:11 +0000

    rest structure

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ to the require section of your `composer.json` file.
 Usage
 -----
 
-Once the extension is installed, simply use it in your code by  :
+Once the extension is installed, simply use it in your code by!1  :
 
 ```php
 <?= \issec2009\api\AutoloadExample::widget(); ?>```
---
+++ src/Api.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace issec2009\api;
+
+use yii\base\BootstrapInterface;
+
+/**
+ * api module definition class
+ */
+class Api extends \yii\base\Module implements BootstrapInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'issec2009\api\controllers';
+//    public $defaultRoute = 'api-driver';
+    public $defaultController = 'api-driver';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+
+//    public function bootstrap($app)
+//    {
+////        $app->getUrlManager()->addRules([
+////            [
+////                'class' => 'yii\rest\UrlRule',
+////                'controller' => ['api-driver'],
+////            ]
+////        ], false);
+//
+//        if ($app instanceof \yii\console\Application) {
+//            $this->controllerNamespace = 'issec2009\api\commands';
+//        }
+//    }
+}
